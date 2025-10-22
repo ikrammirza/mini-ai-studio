2cfed1d645390189fa1b46ce6a64b1e53571acca
ikrammirza <mirzaikram129@gmail.com>
2025-10-22 07:05:15 +0530

    chore: remove backend to isolate frontend for PR

--- backend/src/controllers/authController.ts
+++
@@ -1,5 +0,0 @@
-// Re-exporting controllers for unified routing imports
-import { signupController } from './signupController';
-import { loginController } from './loginController';
-
-export { signupController, loginController };
